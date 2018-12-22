8b835edfc1fc8ad4c8af99095409bb1f55cf0d48
sakura-fly <sourcesakura@sina.com>
2018-12-22 17:57:13 +0800

    大概图和渣渣头部

---
+++ src/test.py
@@ -0,0 +1,29 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QApplication
+from future.moves import sys
+
+
+class Test(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.inintUI()
+
+    def inintUI(self):
+        qv = QVBoxLayout()
+
+        texted1 = QTextEdit()
+        texted2 = QTextEdit()
+
+        texted1.resize(300, 300)
+
+        qv.addWidget(texted1)
+        qv.addWidget(texted2)
+
+        self.setLayout(qv)
+
+        self.show()
+
+
+app = QApplication(sys.argv)
+test = Test()
+sys.exit(app.exec_())
---
+++ src/ui/head.py
@@ -0,0 +1,55 @@
+import sys
+
+from PyQt5.QtGui import QTextLine, QPixmap
+from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QApplication, QLineEdit, QPushButton
+
+
+class Head(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # 左边标题
+        title = QLabel("music")
+
+        # 搜索框
+        search = QLineEdit()
+        # 提示内容
+        search.setPlaceholderText("搜索")
+        search.setMaximumWidth(200)
+        # 绑定回车时间
+        search.returnPressed.connect(self.search)
+
+        # 放了个图片
+        uNameLabel = QLabel("sakura")
+        closeLabel = QLabel()
+        pixmap = QPixmap('F:\\月泉\\test\\ssmusic\\src\\resource\\img\\rin.png') \
+            .scaled(closeLabel.width(), closeLabel.height())
+        closeLabel.setPixmap(pixmap)
+        closeLabel.setMinimumSize(40, 40)
+        closeLabel.setMaximumSize(40, 40)
+
+        # 横布局
+        headLayout = QHBoxLayout()
+        headLayout.addWidget(title)
+        headLayout.addWidget(search)
+
+        headLayout.addStretch(1)
+
+        headLayout.addWidget(uNameLabel)
+        headLayout.addWidget(closeLabel)
+
+        self.setLayout(headLayout)
+
+    # 查询方法
+    def search(self):
+        searchMsg = self.sender().text()
+        print(searchMsg)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    head = Head()
+    head.show()
+    sys.exit(app.exec_())

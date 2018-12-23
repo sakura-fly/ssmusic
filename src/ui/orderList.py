a74c5bffb83fc6320479ef8a4421dd384fc113d7
sakura-fly <sourcesakura@sina.com>
2018-12-23 22:03:01 +0800

    歌曲列表

---
+++ src/ui/orderList.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+from PyQt5.QtGui import QStandardItem, QIcon, QStandardItemModel
+from PyQt5.QtWidgets import QWidget, QListView, QApplication, QMessageBox
+
+
+class OrderList(QListView):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setViewMode(QListView.ListMode)
+        item_1 = QStandardItem(QIcon(os.path.join(os.path.abspath("../../"), "resource", "img", "rin.png")), "普通员工A");
+        item_2 = QStandardItem(QIcon(os.path.join(os.path.abspath("../../"), "resource", "img", "rin.png")), "普通员工B");
+        item_3 = QStandardItem(QIcon(os.path.join(os.path.abspath("../../"), "resource", "img", "rin.png")), "普通员工C");
+        model = QStandardItemModel()
+        model.appendRow(item_1)
+        model.appendRow(item_2)
+        model.appendRow(item_3)
+        self.clicked.connect(self.clickOrder)
+        self.setModel(model)
+
+    def clickOrder(self, index):
+        QMessageBox.information(self, "ListView", "选择项是：%d" % (index.row()))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    orderList = OrderList()
+    orderList.show()
+    sys.exit(app.exec_())
